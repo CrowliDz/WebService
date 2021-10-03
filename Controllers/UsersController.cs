9761fd897e5888d52d7ea6ff0384405098424b4c
Miguel Miranda Colis <44578094+CrowliDz@users.noreply.github.com>
2021-10-02 22:39:38 -0500

    first

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using WebService.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() //IE nos eprmite tratar tipos localmente
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        [Route("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return  _context.Users.Find(id);
+        }
+
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using WebService.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users{ get; set; }
+
+       
+    }
+}
